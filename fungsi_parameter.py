8e1ee89ad8f7025066d6b4bc290b9d46d2be0f5d
KRISNA FEBRIANTI <krisnafebss@gmail.com>
2022-05-27 15:45:11 +0700

    fungsi_parameter

---
+++ fungsi_parameter.py
@@ -0,0 +1,13 @@
+def selamat_datang(nama):
+
+    print('halo',nama,'apa kabar')
+
+selamat_datang('budi')
+
+def greetings(nama,waktu):
+    print('hallo',nama,'selamat',waktu)
+    print('apakah',nama,'sudah datanf',waktu,'ini?')
+
+greetings('andi','pagi')
+greetings('rudi','siang')
+greetings('tika','malam')
